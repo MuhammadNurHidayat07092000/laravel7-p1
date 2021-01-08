68bb67b8fbd2d9d5ca310d2c12a0faeee5a6363e
muhammadnurhidayat07092000 <yyt08536@gmailcom>
2021-01-08 20:17:37 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can re78uiloppppppppppppppppppister web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
@@ -15,4 +15,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('foo', function () {
+    return 'Hello World';
+});
+
+Route::redirect('/here', '/foo');
+
+Route::get('user/{name?}', function ($name = 'John') {
+    return $name;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('users', function () {
+        return "admin > user";
+        // Matches The "/admin/users" URL
+    });
 });
